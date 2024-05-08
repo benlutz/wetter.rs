cd17eb6bdd40566c0c35b1aa4296eb4cf8cbea43
Benjamin Lutz <benni.lutz@gmail.com>
2024-05-08 19:53:01 +0200

    add clap

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,22 @@ mod helpers;
 use city::City;
 mod openweather;
 mod weather;
+use clap::Parser;
+
+/// A simple program to get the current weather of a city.
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(default_value = "Wien")]
+    query: String,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv().ok();
 
-    let query = "Wien";
-    let cities: Vec<City> = openweather::search_for_city(query).await?;
+    let args = Args::parse();
+    let cities: Vec<City> = openweather::search_for_city(&args.query).await?;
 
     if let Some(local_names) = &cities[0].local_names {
         if let Some(name) = local_names.get(&helpers::get_language()) {
